f199cd7722e023fddc4e56e7cbc51eaf5fc7fb16
bella-247 <abelmekonen247@gmail.com>
2024-09-11 05:35:55 +0300

    Refactor navigation links to use anchor tags with class "drop-links" and update font size in style.scss

--- script.js
+++ script.js
@@ -20,18 +20,52 @@ tabLinks.forEach(link => {
     })
 })
 
+const dropLinks = [...document.querySelectorAll("nav .drop-links")]
+
+dropLinks.forEach(link => {
+    link.addEventListener("click", (e) => {
+        // removing the active class of the already activated class
+        document.querySelector(".drop-links.active").classList.remove("active")
+        e.currentTarget.classList.add('active')
+    })
+})
 
 
 
+// using intersection observer to change the active nav link when the specified section is intesecting the view
+const sections = document.querySelectorAll('main section');
+
+function check_current_active_nav_menu(){
+    const tabMenus = document.querySelector("nav .tab-menus");
+    const tabMenusStyle = getComputedStyle(tabMenus)
+    return tabMenusStyle.display == "none" ? ".drop-links" : ".tab-links"
+}
+const observerOptions = {
+    root: null,
+    rootMargin: `-${navBarHeight}px 0px 0px 0px`,
+    threshold: 0.5
+}
 
 
 
+const observerCallback = (entries) =>{
+    entries.forEach(entry =>{
+        const { target, isIntersecting } = entry;
+        const id = target.getAttribute("id");
+        const currentActive = check_current_active_nav_menu();
+        const navLink = document.querySelector(`nav ${currentActive}[href="#${id}"]`);
+
+        if (isIntersecting){
+            document.querySelector(`${currentActive}.active`).classList.remove("active")
+            navLink.classList.add("active");
+        }
+    })
+}
 
 
+const observer = new IntersectionObserver(observerCallback, observerOptions);
 
-
-
-
+sections.forEach(section => observer.observe(section));
 
 
 
